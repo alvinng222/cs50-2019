f98637bbb4da896ac77167f8b694630feb05e159
AlvinNgTW <AlvinNgTW@users.noreply.github.com>
2019-09-03 15:49:50 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- crack.c
+++ crack.c
@@ -14,11 +14,11 @@ bool decrypt(char *h, char *sa); //input of string hash, salt, if true will prin
 
 int main(int argc, char *argv[])
 {
-    if ((argc == 2) && ( strlen(argv[1]) == 13 ))
+    if ((argc == 2) && (strlen(argv[1]) == 13))
     {
         // quatify the 2 chars salt from hash
         char *s = combi_2c(argv[1][0], argv[1][1]);
-        if ( (salt(s[0]) == 0) && (salt(s[1]) == 0) )
+        if ((salt(s[0]) == 0) && (salt(s[1]) == 0))
         {
             if (decrypt(argv[1], s) == 0)
             {
@@ -43,7 +43,8 @@ char *combi_2c(char x, char y)
     // Allocate memory for another string
     char *t = malloc((2 + 1) * sizeof(char)); // malloc for 2 char
     // Chance first letter in string
-    t[0] = x; t[1] = y;
+    t[0] = x; 
+    t[1] = y;
     return t;
 }
 
@@ -57,17 +58,23 @@ bool salt(char a)
     for (int i = 0, n = strlen(slib1); i < n; i++)
     { 
         if (a == slib1[i]) 
+        {
             return 0;
+        }
     }
     for (int i = 0, n = strlen(slib2); i < n; i++)
     { 
         if (a == slib2[i]) 
+        {
             return 0;
+        }
     }
     for (int i = 0, n = strlen(slib3); i < n; i++)
     {
         if (a == slib3[i]) 
+        {
             return 0;
+        }
     }
     return 1;
 }
@@ -91,26 +98,66 @@ bool decrypt(char *h, char *sa)
         // maximum ways of alpha chars, power of no. of chars of keywords 
         int max_bits = al;
         for (int x = 1; x < k; x++)
-            max_bits = max_bits*al;
+        {
+            max_bits = max_bits * al;
+        }
         //printf("max_bits: %i\n", max_bits);  
         int test = 0, test1 = 0; // for trouble shooting
         
         // while loop for increment for individual chars of keyword
         while (bits < max_bits) 
         {
-            if (d0 >= al) {d0 = 0; d1++;}  
-            if (k > 1 && d1 >= al) {d1 = 0; d2++;}  // for 2+ chars
-            if (k > 2 && d2 >= al) {d2 = 0; d3++;}  // for 3+ chars
-            if (k > 3 && d3 >= al) {d3 = 0; d4++;}  // for 4+ chars
+            if (d0 >= al) 
+            {
+                d0 = 0; 
+                d1++;
+            }  
+            if (k > 1 && d1 >= al) 
+            {
+                d1 = 0; 
+                d2++;    // for 2+ chars
+            } 
+            if (k > 2 && d2 >= al) 
+            {
+                d2 = 0; 
+                d3++;    // for 3+ chars
+            }  
+            if (k > 3 && d3 >= al) 
+            {
+                d3 = 0; 
+                d4++;     // for 4+ chars
+            } 
 
-            if (k == 2){t[0] = ap[d0]; t[1] = ap[d1];}
-            if (k == 3){t[0] = ap[d0]; t[1] = ap[d1]; t[2] = ap[d2];}
-            if (k == 4){t[0] = ap[d0]; t[1] = ap[d1]; t[2] = ap[d2]; t[3] = ap[d3];}
-            if (k == 5){t[0] = ap[d0]; t[1] = ap[d1]; t[2] = ap[d2]; t[3] = ap[d3]; t[4] = ap[d4];}
+            if (k == 2)
+            {
+                t[0] = ap[d0]; 
+                t[1] = ap[d1];
+            }
+            if (k == 3)
+            {
+                t[0] = ap[d0]; 
+                t[1] = ap[d1]; 
+                t[2] = ap[d2];
+            }
+            if (k == 4)
+            {
+                t[0] = ap[d0]; 
+                t[1] = ap[d1]; 
+                t[2] = ap[d2]; 
+                t[3] = ap[d3];
+            }
+            if (k == 5)
+            {
+                t[0] = ap[d0]; 
+                t[1] = ap[d1]; 
+                t[2] = ap[d2]; 
+                t[3] = ap[d3]; 
+                t[4] = ap[d4];
+            }
             
             // compare the crypt result against input
             char *thash = crypt(t, sa); 
-            if  (strcmp(thash, h) == 0) 
+            if (strcmp(thash, h) == 0) 
             {
                 printf("%s \n", t); // print out the malloc string
                 free(t);
@@ -146,4 +193,8 @@ $ ./crack 51v3Nh6ZWGHOQ > ROFL
 $ ./crack 61v1CDwwP95bY > hola
 $ ./crack 508ny6Rw0aRio > sean
 $ ./crack 50cI2vYkF0YU2 > LOL
+
+$ check50 cs50/problems/2019/x/crack
+:) crack submitted
+style50 crack.c
 */
