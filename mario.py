9343f6c4ec287c6f022f891d105ddf448d05b27a
AlvinNgTW <AlvinNgTW@users.noreply.github.com>
2019-10-03 09:38:00 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -6,7 +6,7 @@ from cs50 import get_int
 # prompt user for positive number
 while True:
     height = get_int("Height: ")
-    if height > 1 and height < 8:
+    if height > 0 and height < 9:
         break
 
 # print out the column and rows
