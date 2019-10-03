56e9be0fdf2f47976e02a3a131a18e4f45f191e3
AlvinNgTW <AlvinNgTW@users.noreply.github.com>
2019-10-03 09:54:43 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,35 @@
+# /more/mario.py
+# Oct 3, 2019
+
+from cs50 import get_int
+
+# prompt user for positive number
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
+
+# print out the column and rows
+for i in range(height):
+    for j in range(height):
+        # print the space follow by #
+        if j < (height - i - 1):
+            print(" ", end='')
+        else:
+            print("#", end='')
+        # print return to prompt
+    print("  ", end='')
+
+    for k in range(i + 1):
+        print("#", end='')
+    print("")
+
+# $ python mario.py
+# Height: -5
+# Height: five
+# Height: 24
+# Height: 4
+#    #  #
+#   ##  ##
+#  ###  ###
+# ####  ####
